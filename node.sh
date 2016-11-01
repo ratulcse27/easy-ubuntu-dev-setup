b5f4423062318beb9fa1841975dfe12bbcf0e673
Yousuf Khan Ratul <yousuf@student.sust.edu>
2016-11-01 12:41:27 +0600

    add color

--- node.sh
+++ node.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-echo "Are you sure, you want to do enter into this JS S***"
+echo "\e[5m\e[37mAre you sure, you want to do enter into this JS S***"
 echo -e "Maintained By SUST CSE Developer Network (SCDN)\n"
 
 
 echo "Steap:1 [System Update]"
 echo "Update Starts....."
 sudo apt-get update
-echo -e "System Update Completed Successfully\n"
+echo -e "\e[97System Update Completed Successfully\n"
 
 
 echo "Step:2 [Install NVM]"
 sudo apt-get install build-essential
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 nvm install node
-echo -e "NVM with latest node version Installation Completed Successfully\n"
-echo -e "For further info on NVM features https://goo.gl/Xuxmbx \n"
+echo -e "\e[92mNVM with latest node version Installation Completed Successfully\n"
+echo -e "\e[37mFor further info on NVM features \e[96mhttps://goo.gl/Xuxmbx \n"
 
 
 echo "Step:2 [Install Yarn]"
 sudo apt-key adv --keyserver pgp.mit.edu --recv D101F7899D41F3C3
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
-echo -e "Yarn Installation Completed Successfully\n"
-echo -e "For further info on Yarn https://goo.gl/VprTir \n"
+echo -e "\e[92mYarn Installation Completed Successfully\n"
+echo -e "\e[37mFor further info on Yarn \e[96mhttps://goo.gl/VprTir \n"
